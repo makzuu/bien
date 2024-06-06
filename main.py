51a6b0dddfb2771a528f7452f43b653aa539fd40
makzuu <mr.greenmousth@gmail.com>
2024-06-06 14:30:10 -0400

    add 'token_helper.py'
    - move token manipulation functionality to 'token_helper.py'
    - improve token manipulation logic

--- main.py
+++ main.py
@@ -1,71 +1,13 @@
-import secrets
-from dotenv import client_id
-import requests
-import json
 from bridge import *
 
-def get_new_code_verifier():
-    token = secrets.token_urlsafe(100)
-    return token[:128]
+def main():
+    p = ArgP()
+    p.add_arg("command", pos=1, choices=["search", "modify", "add", "me", "mal"], required=True)
+    p.add_arg("arg", pos=2, required=True, not_required_in=["me", "mal"])
+    p.add_arg("--limit", value=True)
+    p.add_arg("--offset", value=True)
+    p.add_arg("--status", value=True)
+    p.add_arg("--num-episodes-watched", value=True)
+    p.add_arg("--score", value=True)
 
-def print_auth_url(code_challenge):
-    base_url = "https://myanimelist.net/v1/oauth2/authorize"
-    response_type = "code"
-    url = f"{base_url}?response_type={response_type}&client_id={client_id}&code_challenge={code_challenge}"
-
-    print(f"click here to login: {url}")
-
-def get_token():
-    code_verifier = code_challenge = get_new_code_verifier()
-    print_auth_url(code_challenge)
-    auth_code = input("insert auth code: ")
-    
-    base_url = "https://myanimelist.net/v1/oauth2/token"
-    data = { "client_id": client_id, "code": auth_code, "code_verifier": code_verifier, "grant_type": "authorization_code" }
-    r = requests.post(base_url, data=data)
-    token = r.json()
-
-    return token
-
-def save_token(token, file_name="token.json"):
-    with open(file_name, "w") as f:
-        json.dump(token, f)
-
-def load_token(file_name="token.json"):
-    with open(file_name, "r") as f:
-        token = json.load(f)
-    return token
-
-if __name__ == "__main__":
-    try:
-        token = load_token()
-    except FileNotFoundError:
-        token = get_token()
-        save_token(token)
-
-    #data, status_code = me(token)
-    #if data is not None:
-    #    print(f"Welcome! {data['name']}")
-
-    #data, status_code = get_anime_list(token)
-    #if status_code == 200:
-    #    print("Anime List:")
-    #    for element in data["data"]:
-    #        node = element["node"]
-    #        status = element["list_status"]["status"]
-    #        print(f"\t({node['id']}) |{status}| {node['title']}")
-
-    data, status_code = search(token, "no game no life", 3)
-    if status_code == 200:
-        print("Results for search q='no game no life'")
-        for element in data["data"]:
-            node = element["node"]
-            print(f"ID: {node['id']}")
-            print(f"Name: {node['title']}")
-            print("Alt Names:")
-            print(f"\t- {node['alternative_titles']['en']}")
-            print(f"\t- {node['alternative_titles']['ja']}")
-            print(f"Airing status: {node['status']}")
-            print("---\n")
-    else:
-        print(status_code, data)
+    args = p.get_args()
---
+++ token_helper.py
@@ -0,0 +1,91 @@
+import requests
+import json
+from dotenv import client_id
+import secrets
+import os
+import time
+
+def get_new_code_verifier():
+    token = secrets.token_urlsafe(100)
+    return token[:128]
+
+def get_auth_code(code_challenge):
+    base_url = "https://myanimelist.net/v1/oauth2/authorize"
+    response_type = "code"
+    url = f"{base_url}?response_type={response_type}&client_id={client_id}&code_challenge={code_challenge}"
+
+    print(f"click here to get your auth code: {url}")
+    return input("insert your auth code here: ")
+
+def request_user_access_token(auth_code, code_verifier):
+    base_url = "https://myanimelist.net/v1/oauth2/token"
+    data = { "client_id": client_id, "code": auth_code, "code_verifier": code_verifier, "grant_type": "authorization_code" }
+    r = requests.post(base_url, data=data)
+    if r.status_code != 200:
+        raise Exception("Error: Could not retrieve user access token")
+
+    return r.json()
+
+def refresh_user_access_token(refresh_token):
+    base_url = "https://myanimelist.net/v1/oauth2/token"
+    data = { "client_id": client_id, "grant_type": "refresh_token", "refresh_token": refresh_token }
+    r = requests.post(base_url, data=data)
+    if r.status_code != 200:
+        raise Exception("Error: Could not refresh user access token")
+    return r.json()
+
+def get_access_token_status(expires_in, file_path="token.json"):
+    last_m = os.path.getmtime(file_path)
+    time_now = time.time()
+    time_left = last_m + expires_in
+    if time_left > time_now:
+        days_left = (time_left - time_now) / 60 / 60 / 24
+        if days_left < 10:
+            expired = False
+            must_refresh = True
+        else:
+            expired = False
+            must_refresh = False
+    else:
+        expired = True
+        must_refresh = False
+
+    return expired, must_refresh
+
+def get_token():
+    try:
+        token = load_token()
+        expired, must_refresh = get_access_token_status(token["expires_in"])
+        if expired:
+            code_verifier = code_challenge = get_new_code_verifier()
+            auth_code = get_auth_code(code_challenge)
+            token = request_user_access_token(auth_code, code_verifier)
+            save_token(token)
+        elif must_refresh:
+            token = refresh_user_access_token(token["refresh_token"])
+            save_token(token)
+        else:
+            return token
+
+        return token
+
+    except FileNotFoundError:
+        code_verifier = code_challenge = get_new_code_verifier()
+        auth_code = get_auth_code(code_challenge)
+        token = request_user_access_token(auth_code, code_verifier)
+        save_token(token)
+        return token
+    raise Exception("Error: You're not supposed to be here")
+
+def save_token(token, file_name="token.json"):
+    with open(file_name, "w") as f:
+        json.dump(token, f)
+
+def load_token(file_name="token.json"):
+    with open(file_name, "r") as f:
+        token = json.load(f)
+    return token
+
+if __name__ == "__main__":
+    token = get_token()
+    print(token)
