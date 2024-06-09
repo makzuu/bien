87a96d4753aacbe6a795653e5c4ff667e976d925
makzuu <mr.greenmousth@gmail.com>
2024-06-09 11:46:08 -0400

    update "main.py"

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from bridge import *
+from args_p import ArgP
+from token_helper import get_token
 
 def main():
     p = ArgP()
@@ -11,3 +13,39 @@ def main():
     p.add_arg("--score", value=True)
 
     args = p.get_args()
+
+    token = get_token()
+    if args["command"] == "search":
+        anime_name = args["arg"]
+        limit = args.get("--limit", 3)
+        offset = args.get("--offset", 0)
+
+        content, status_code = search(token, anime_name, limit, offset)
+        print(f"{status_code}\n{content}")
+    elif args["command"] == "add":
+        anime_id = args["arg"]
+        status = args.get("--status")
+
+        content, status_code = add(token, anime_id, status)
+        print(f"{status_code}\n{content}")
+    elif args["command"] == "modify":
+        anime_id = args["arg"]
+        status = args.get("--status")
+        score = args.get("--score")
+        num_watched_episodes = args.get("--num_watched_episodes")
+
+        content, status_code = modify(token, anime_id, status, score, num_watched_episodes)
+        print(f"{status_code}\n{content}")
+    elif args["command"] == "mal":
+        limit = args.get("--limit")
+        offset = args.get("--offset")
+        status = args.get("--status")
+
+        content, status_code = get_my_anime_list(token, limit, offset, status)
+        print(f"{status_code}\n{content}")
+    elif args["command"] == "me":
+        content, status_code = me(token)
+        print(f"{status_code}\n{content}")
+
+
+main()
